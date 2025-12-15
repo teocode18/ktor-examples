b82754608439ee4abbd1e008633b5aa8edbf3e26
Nick Efford <nick.efford@gmail.com>
2025-12-15 11:41:07 +0000

    music-club: set default DB in server application config

--- music-club/server/src/comp2850/music/server/Application.kt
+++ music-club/server/src/comp2850/music/server/Application.kt
@@ -5,15 +5,22 @@ package comp2850.music.server
 import comp2850.music.db.MusicDatabase
 import comp2850.music.db.TestDatabase
 import io.ktor.server.application.Application
+import org.jetbrains.exposed.v1.jdbc.transactions.TransactionManager
+
+// Main config for application
 
 fun Application.module() {
+    TransactionManager.defaultDatabase = MusicDatabase.db
     configureTemplates()
-    configureRouting(MusicDatabase.db)
+    configureRouting()
 }
 
+// Alternative config, for use in tests
+
 fun Application.testModule() {
+    TransactionManager.defaultDatabase = TestDatabase.db
     configureTemplates()
-    configureRouting(TestDatabase.db)
+    configureRouting()
 }
 
 fun main(args: Array<String>) {
--- music-club/server/src/comp2850/music/server/Routing.kt
+++ music-club/server/src/comp2850/music/server/Routing.kt
@@ -7,9 +7,8 @@ import io.ktor.server.application.Application
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
-import org.jetbrains.exposed.v1.jdbc.Database
 
-fun Application.configureRouting(db: Database) {
+fun Application.configureRouting() {
     routing {
         get("/") { call.homePage() }
         post("/search") { call.searchResults() }
